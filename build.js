d354f394abc1d44a2f3e9314e519ef4fe9824637
Ivo Wetzel <ivo.wetzel@googlemail.com>
2013-04-04 00:40:14 +0200

    Fix argument lists

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    util = require('util'),
     jison = require('jison');
 
 var lexer = require('./lib/lexer'),
@@ -22,7 +23,7 @@ console.log('Generated Parser into "lib/parser.js"');
 // Some simple tests
 var parser = require('./lib/parser');
 
-var code = '~!(+true) + (-2) * (++e) / 5 * foo()[2..(1 - 3)] + @foo.bar[2 + 2] - (a ? b : c) // 2 + @ + a > b && c < d || 4 != 5 - foo(1, 2, 3, foo = 2 + 2, bla...)';
-var code = 'foo.bar[2](2 * 2)';
-console.log(parser.parse(code));
+//var code = '~!(+true) + (-2) * (++e) / 5 * foo()[2..(1 - 3)] + @foo.bar[2 + 2] - (a ? b : c) // 2 + @ + a > b && c < d || 4 != 5 - foo(1, 2, 3, foo = 2 + 2, bla...)';
+var code = 'foo.bar[2](2 * 2, foo, 4)';
+console.log(util.inspect(parser.parse(code), false, 4));
 
